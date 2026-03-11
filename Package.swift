1a9bc9506cf89f65ecad8e2923e22e22162ca318
Suneel <developer@apprikart.com>
2026-03-11 13:18:41 +0530

    Package updated

--- Package.swift
+++ Package.swift
@@ -27,7 +27,11 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "ApprikVCKit"
+            name: "ApprikVCKit",
+            dependencies: [ .product(name: "Starscream", package: "Starscream"),
+                            .product(name: "SwiftMessages", package: "SwiftMessages")
+            ],
+            path: "Sources/ApprikVCKit"
         ),
 
     ]
