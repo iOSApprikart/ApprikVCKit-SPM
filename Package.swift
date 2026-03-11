08b89bac6fa18c2626ef2f7e41949db821a343f3
Suneel <developer@apprikart.com>
2026-03-11 13:05:28 +0530

    Package details are updated

--- Package.swift
+++ Package.swift
@@ -1,10 +1,11 @@
-// swift-tools-version: 6.2
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "ApprikVCKit",
+    platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -12,6 +13,16 @@ let package = Package(
             targets: ["ApprikVCKit"]
         ),
     ],
+    dependencies: [
+           .package(
+               url: "https://github.com/daltoniam/Starscream.git",
+               from: "4.0.6"
+           ),
+           .package(
+               url: "https://github.com/SwiftKickMobile/SwiftMessages.git",
+               from: "10.0.0"
+           )
+       ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
--- Sources/ApprikVCKit/ApprikVCKit.swift
+++ Sources/ApprikVCKit/ApprikVCKit.swift
@@ -1,2 +1,12 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Foundation
+
+public struct TestSPM {
+    public init() {}
+    
+    public static func hello() -> String {
+        return "Hello, World!"
+    }
+}
