04b94b0a91b89d053d473cc94396ba99a9efcae0
Suneel <developer@apprikart.com>
2026-03-11 15:47:30 +0530

    updated code

--- Package.swift
+++ Package.swift
@@ -20,14 +20,17 @@ let package = Package(
            )
        ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
+        .binaryTarget(
+            name: "ApprikVCKitBinary",
+            path: "./ApprikVCKit.xcframework"
+        ),
         .target(
             name: "ApprikVCKit",
-            dependencies: [ .product(name: "appwebrtc-spm", package: "appwebrtc-spm")
+            dependencies: [
+                "ApprikVCKitBinary",
+                .product(name: "AppWebRTC", package: "AppWebRTC-SPM")
             ],
             path: "Sources/ApprikVCKit"
-        ),
-
+        )
     ]
 )
